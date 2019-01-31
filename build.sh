b9c34f45f5d993fd9b0a8c9bd0383cb8b9ea1452
Rule303 <ruletot@gmail.com>
2019-01-30 22:40:02 -0500

    leave out the build.sh script from mod build

--- build.sh
+++ build.sh
@@ -4,8 +4,12 @@
 
 FILES=$(git diff --name-only `git merge-base origin/master HEAD`)
 while read -r line; do
-    echo "$line"
-    path=$( echo ${line%/*} )
-    mkdir -p "c:\Steam\steamapps\common\Battle Brothers\data\\$path"
-    cp "$line" "c:\Steam\steamapps\common\Battle Brothers\data\\$line"
+    if [ "$line" == "build.sh" ]; then
+        echo "skipping $line"
+    else 
+        echo "$line"
+        path=$( echo ${line%/*} )
+        mkdir -p "c:\Steam\steamapps\common\Battle Brothers\data\\$path"
+        cp "$line" "c:\Steam\steamapps\common\Battle Brothers\data\\$line"
+    fi
 done <<< "$FILES"
