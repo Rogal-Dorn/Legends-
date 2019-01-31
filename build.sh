4e6eb487d4ac3cc301e2dcad364d7e292bb412ca
Rule303 <ruletot@gmail.com>
2019-01-30 22:18:43 -0500

    use windows line path

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 FILES=$(git diff --name-only `git merge-base origin/master HEAD`)
 while read -r line; do
     echo "$line"
-    path=$( echo ${line%\\*} )
+    path=$( echo ${line%/*} )
     mkdir -p "c:\Steam\steamapps\common\Battle Brothers\data\\$path"
     cp "$line" "c:\Steam\steamapps\common\Battle Brothers\data\\$line"
 done <<< "$FILES"
