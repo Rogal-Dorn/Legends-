150928017aa3d6c7d9e370c2c9d13b247e28ff79
Rule303 <ruletot@gmail.com>
2019-01-30 22:14:11 -0500

    simple build script that copies all files changed in the mod into the BB install directory

---
+++ build.sh
@@ -0,0 +1,11 @@
+# declare -a scriptFiles=(
+#     "scripts/config/actor.nut"
+# )
+
+FILES=$(git diff --name-only `git merge-base origin/master HEAD`)
+while read -r line; do
+    echo "$line"
+    path=$( echo ${line%\\*} )
+    mkdir -p "c:\Steam\steamapps\common\Battle Brothers\data\\$path"
+    cp "$line" "c:\Steam\steamapps\common\Battle Brothers\data\\$line"
+done <<< "$FILES"
