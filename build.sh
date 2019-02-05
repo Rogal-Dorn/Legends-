2e1bdd0196cb7c9c6a9f0546cc44d26ac390c3c2
Rule303 <ruletot@gmail.com>
2019-02-05 07:18:14 -0500

    update build script to skip releasenotes.md

--- build.sh
+++ build.sh
@@ -6,7 +6,9 @@ FILES=$(git diff --name-only `git merge-base origin/master HEAD`)
 while read -r line; do
     if [ "$line" == "build.sh" ]; then
         echo "skipping $line"
-    else 
+    elif [ "$line" == "ReleaseNotes.md" ]; then
+        echo "skipping $line"
+    else
         echo "$line"
         path=$( echo ${line%/*} )
         mkdir -p "c:\Steam\steamapps\common\Battle Brothers\data\\$path"
