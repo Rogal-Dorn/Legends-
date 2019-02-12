e7f1b6f34b99c8a4126d49437691b3ea28ece23c
Rule303 <ruletot@gmail.com>
2019-02-11 21:48:01 -0500

    add build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+
+FILES=$(git diff --name-only `git merge-base origin/master HEAD`)
+while read -r line; do
+    if [ "$line" == "build.sh" ]; then
+        echo "skipping $line"
+    elif [ "$line" == "ReleaseNotes.md" ]; then
+        echo "skipping $line"
+    else
+        echo "$line"
+        path=$( echo ${line%/*} )
+        mkdir -p "c:\Steam\steamapps\common\Battle Brothers\data\\$path"
+        cp "$line" "c:\Steam\steamapps\common\Battle Brothers\data\\$line"
+    fi
+done <<< "$FILES"
