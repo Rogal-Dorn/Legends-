8968fd42ee9ace3b5c9eb7f07ffb5e411cd1931d
Rule303 <ruletot@gmail.com>
2019-07-24 19:57:02 -0400

    port over v13 build setup

--- build_compile.sh
+++ build_compile.sh
@@ -1,18 +1,21 @@
-
 FILES=$(git diff --name-only `git merge-base origin/master HEAD`)
 while read -r line; do
-    if [ "$line" == "build.sh" ]; then
+    if [[ "$line" == *.sh ]]; then
+        echo "skipping $line" 
+    elif [[ "$line" == *.md ]]; then
         echo "skipping $line"
-    elif [ "$line" == "unstaged_build.sh" ]; then
+    elif [[ "$line" == unpacked* ]]; then
         echo "skipping $line"
-    elif [ "$line" == "unstaged_build_poss.sh" ]; then
+    elif [[ "$line" == gfx/*.png ]]; then
         echo "skipping $line"
-    elif [ "$line" == "build_compile.sh" ]; then
-        echo "skipping $line"                   
-    elif [ "$line" == "build_compile_poss.sh" ]; then
-        echo "skipping $line"                   
-    elif [ "$line" == "ReleaseNotes.md" ]; then
+    elif [[ "$line" == gfx/*.jpg ]]; then
         echo "skipping $line"
+    elif [[ "$line" == sounds/*.wav ]]; then
+        echo "skipping $line"
+    elif [[ "$line" == *.cnut ]]; then
+        echo "skipping $line"
+    elif [ "$line" == ".gitignore" ]; then
+        echo "skipping $line"  
     elif [ "$line" == "scripts/mapgen/templates/world/worldmap_generator.nut" ]; then
         echo "skipping $line"  
     elif [ "$line" == "scripts/contracts/contract.nut" ]; then
@@ -32,9 +35,7 @@ while read -r line; do
     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_hook.nut" ]; then
         echo "skipping $line"  
     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_charm.nut" ]; then
-        echo "skipping $line"
-    elif [ "$line" == "scripts/root_state.cnut" ]; then
-        echo "skipping $line"          
+        echo "skipping $line"       
     else
         echo "$line"
         path=$( echo ${line%/*} )
@@ -43,6 +44,19 @@ while read -r line; do
     fi
 done <<< "$FILES"
 
+mkdir -p "brushes"
 cd ../bin
+./bbrusher.exe pack --gfxPath "../battlebrothers/" ../battlebrothers/brushes/entity_blood.brush ../battlebrothers/unpacked/entity_blood
+./bbrusher.exe pack --gfxPath "../battlebrothers/" ../battlebrothers/brushes/legend_characters.brush ../battlebrothers/unpacked/legend_characters
+./bbrusher.exe pack --gfxPath "../battlebrothers/" ../battlebrothers/brushes/legend_enemies.brush ../battlebrothers/unpacked/legend_enemies
+./bbrusher.exe pack --gfxPath "../battlebrothers/" ../battlebrothers/brushes/legend_runed.brush ../battlebrothers/unpacked/legend_runed
+./bbrusher.exe pack --gfxPath "../battlebrothers/" ../battlebrothers/brushes/legend_weapons.brush ../battlebrothers/unpacked/legend_weapons
+./bbrusher.exe pack --gfxPath "../battlebrothers/" ../battlebrothers/brushes/legend_world.brush ../battlebrothers/unpacked/legend_world
+./bbrusher.exe pack --gfxPath "../battlebrothers/" ../battlebrothers/brushes/legends_ui.brush ../battlebrothers/unpacked/legends_ui
+./bbrusher.exe pack --gfxPath "../battlebrothers/" ../battlebrothers/brushes/legend_horses.brush ../battlebrothers/unpacked/legend_horses
 ./masscompile.bat "c:\Steam\steamapps\common\Battle Brothers\data\scripts"
 cd ../battlebrothers
+
+cp -R sounds/. "c:\Steam\steamapps\common\Battle Brothers\data\sounds"
+cp -R gfx/. "c:\Steam\steamapps\common\Battle Brothers\data\gfx"
+cp -R brushes/. "c:\Steam\steamapps\common\Battle Brothers\data\brushes"
