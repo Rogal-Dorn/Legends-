50343218f0689d4baac5f1f09d72d4af9abf01b2
Rule303 <ruletot@gmail.com>
2019-07-24 19:57:02 -0400

    port over v13 build setup

---
+++ build_brushes_poss.sh
@@ -0,0 +1,11 @@
+mkdir -p "brushes"
+cd ../bin
+./bbrusher.exe pack --gfxPath "../Repo/" ../Repo/brushes/entity_blood.brush ../Repo/unpacked/entity_blood
+./bbrusher.exe pack --gfxPath "../Repo/" ../Repo/brushes/legend_characters.brush ../Repo/unpacked/legend_characters
+./bbrusher.exe pack --gfxPath "../Repo/" ../Repo/brushes/legend_enemies.brush ../Repo/unpacked/legend_enemies
+./bbrusher.exe pack --gfxPath "../Repo/" ../Repo/brushes/legend_runed.brush ../Repo/unpacked/legend_runed
+./bbrusher.exe pack --gfxPath "../Repo/" ../Repo/brushes/legend_weapons.brush ../Repo/unpacked/legend_weapons
+./bbrusher.exe pack --gfxPath "../Repo/" ../Repo/brushes/legend_world.brush ../Repo/unpacked/legend_world
+./bbrusher.exe pack --gfxPath "../Repo/" ../Repo/brushes/legends_ui.brush ../Repo/unpacked/legends_ui
+./bbrusher.exe pack --gfxPath "../Repo/" ../Repo/brushes/legend_horses.brush ../Repo/unpacked/legend_horses
+cd ../Repo
