83358fb6b3f19d3c7e65eb63e6dbb8c194b7a93e
Superposs <possum.hodgkin@gmail.com>
2020-02-18 21:20:58 +1100

    May be the last key to fixing rotation

--- build_compile_poss.sh
+++ build_compile_poss.sh
@@ -48,6 +48,8 @@ while read -r line; do
         echo "skipping $line"
     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_charm.nut" ]; then
         echo "skipping $line"
+    elif [ "$line" == "scripts/ai/tactical/behaviors/ai_defend_rotation.nut" ]; then
+        echo "skipping $line"
     elif [ "$line" == "scripts/contracts/contracts/barbarian_king_contract" ]; then
         echo "skipping $line"
     elif [ "$line" == "scripts/factions/faction_action.nut" ]; then
