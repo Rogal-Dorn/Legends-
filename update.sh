0f61f37e114f56dcff0adedfaef71116cb4e4d70
Rule303 <ruletot@gmail.com>
2020-08-14 23:42:45 -0400

    update.sh

--- update.sh
+++ update.sh
@@ -4,8 +4,8 @@ unzip "c:\Steam\steamapps\common\Battle Brothers\data\data_001.dat" -d "tmp"
 cd ../bin
 ./massdecompile.bat "..\battlebrothers\tmp"
 cd ../battlebrothers
-cp tmp/ui .
-cp tmp/scripts .
+cp -r tmp/ui .
+cp -r tmp/scripts .
 rm -rf tmp
 git status
 echo DONE
