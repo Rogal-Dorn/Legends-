7ccf604a2ce2f0a21d9c3a7b3d920300926cbb0c
Rule303 <ruletot@gmail.com>
2020-11-22 22:12:12 -0500

    splits up helmet layers into smaller brush files

--- build_brushes_poss.sh
+++ build_brushes_poss.sh
@@ -12,6 +12,8 @@ python ../Repo/unpacked/make_legend_helmets.py
 ./bbrusher.exe pack --gfxPath "../Repo/" ../Repo/brushes/legend_helmets.brush ../Repo/unpacked/legend_helmets
 ./bbrusher.exe pack --gfxPath "../Repo/" ../Repo/brushes/legend_helmets_0.brush ../Repo/unpacked/legend_helmets/0
 ./bbrusher.exe pack --gfxPath "../Repo/" ../Repo/brushes/legend_helmets_1.brush ../Repo/unpacked/legend_helmets/1
+./bbrusher.exe pack --gfxPath "../Repo/" ../Repo/brushes/legend_helmets_2.brush ../Repo/unpacked/legend_helmets/2
+./bbrusher.exe pack --gfxPath "../Repo/" ../Repo/brushes/legend_helmets_3.brush ../Repo/unpacked/legend_helmets/3
 python ../Repo/unpacked/make_legend_armor.py
 ./bbrusher.exe pack --gfxPath "../Repo/" ../Repo/brushes/legend_armor.brush ../Repo/unpacked/legend_armor
 ./bbrusher.exe pack --gfxPath "../Repo/" ../Repo/brushes/terrain.brush ../Repo/unpacked/terrain
