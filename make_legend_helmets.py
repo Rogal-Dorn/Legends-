cf415fef4fb0e1c6b0f7fa9ade64f924271ed64b
p0ss <possum.hodgkin@gmail.com>
2023-07-20 18:59:25 +1000

    Optimised brush configuration to efficiently produce 2024px brushes for helmets and armors

--- make_legend_helmets.py
+++ make_legend_helmets.py
@@ -1939,7 +1939,7 @@ def makeBrushes():
                 text.replace("/", "\\")
                 F.write(text)
                 imageCount += 1
-                if (imageCount > 700):
+                if (imageCount > 150):
                     F.write('</brush>\n')
                     F.close()
                     imageCount = 0
@@ -1975,7 +1975,7 @@ def makeBrushes():
                 text.replace("/", "\\")
                 F.write(text)
                 imageCount += 1
-                if (imageCount > 2000):
+                if (imageCount > 150):
                     F.write('</brush>\n')
                     F.close()
                     imageCount = 0
