1503d8af6cda43db80cb4d8eec3c75f1c817ae96
Merucyr <mercury.raleigh@gmail.com>
2023-09-15 08:27:09 -0400

    mwah script again haha

--- bcompile_mwah.sh
+++ bcompile_mwah.sh
@@ -1,2 +1,22 @@
-./build_legends_mod.sh "c:\\Program Files (x86)\\Steam\\steamapps\\common\\Battle Brothers\\data" "legends"
+BBDir="${1-"c:\\Program Files (x86)\\Steam\\steamapps\\common\\Battle Brothers\\data"}"
+RepoDir="${2-"legends"}"
+
+rm -rf  "$BBDir\\brushes" "$BBDir\\gfx" "$BBDir\\preload" "$BBDir\\scripts" "$BBDir\\sounds" "$BBDir\\ui"
+
+./build_legends_mod.sh "$BBDir" "$RepoDir"
+resp="$?"
+echo $resp
+if [ $resp -ne "0" ]
+then
+    echo "Failed to build Legends!"
+    exit 1
+fi
+
+echo "Zipping mod ..."
+rm -rf "$BBDir\\mod_legends.zip"
+"C:\Program Files\7-Zip\7z.exe" a "$BBDir\\mod_legends.zip" "$BBDir\\brushes" "$BBDir\\gfx" "$BBDir\\preload" "$BBDir\\scripts" "$BBDir\\sounds" "$BBDir\\ui"
+echo "Cleaning up ..."
+rm -rf  "$BBDir\\brushes" "$BBDir\\gfx" "$BBDir\\preload" "$BBDir\\scripts" "$BBDir\\sounds" "$BBDir\\ui"
+mkdir -p "$BBDir\\gfx"
+cp -R ../fonts "$BBDir\\gfx"
 $SHELL
