58418cd107bb9b411405dd92dff2d66a2f4b1419
Hanter <hanter19.dev@gmail.com>
2023-11-03 03:17:57 +0800

    Even more Basilisk fixes
    - Basilisk head, body, and injured sprites are now proportionate
      - The trick was to edit the images so that both the head and body images have the same dimensions and fit perfectly when overlayed. This way, no `left/right/top/bottom` configurations were necessary in the brush metadata
      - The injury brush needs cover both the body and head, so I combined both unpacked images into one. The sprite needs to be added after the head, so that it will be layered on top of both the body and the head
    - Basilisk corpse and dead head brushes untouched and still need adjusting
    - Move `actives.legend_basilisk_peck` skill out of `ai_attack_default.nut` and into `mod_ai_add_legend_skills.nut` instead (the former actually still works, but I'm moving it anyway for consistency with other legends-added skills)
    - Add overlay brush for basilisk peck skill

--- make_legend_enemies.py
+++ make_legend_enemies.py
@@ -145,12 +145,11 @@ enemies = r"""
 <sprite id="bust_stollwurm_body_01_dead_arrows" offsetX="5" offsetY="20" f="64FB" ic="FF474C4F" width="179" height="154" img="entity\beasts\bust_stollwurm_body_01_dead_arrows.png" left="-31" right="47" top="-2" bottom="48" />
 <sprite id="bust_stollwurm_body_01_dead_javelin" offsetX="5" offsetY="20" f="64FB" ic="FF141B27" width="179" height="154" img="entity\beasts\bust_stollwurm_body_01_dead_javelin.png" left="-55" right="41" top="13" bottom="75" />
 
-<sprite id="basilisk_drone_01" offsetX="-2" offsetY="45" ic="FF24554E" width="104" height="142" img="entity\beasts\basilisk_drone_01.png" left="-78" right="47" top="-69" bottom="71" />
+<sprite id="basilisk_drone_01" offsetX="-15" offsetY="45" ic="FF24554E" width="104" height="142" img="entity\beasts\basilisk_drone_01.png" />
+<sprite id="basilisk_drone_head_01" offsetX="-15" offsetY="45" ic="FF315B61" width="104" height="142" img="entity\beasts\basilisk_drone_head_01.png" />
+<sprite id="basilisk_drone_01_injured" offsetX="-15" offsetY="45" ic="FF5E9C89" width="104" height="142" img="entity\beasts\basilisk_drone_01_injured.png" />
 <sprite id="basilisk_drone_01_dead" offsetX="5" offsetY="20" f="64FF" ic="FF295850" width="179" height="154" img="entity\beasts\basilisk_drone_01_dead.png" left="-74" right="76" top="-50" bottom="48" />
-<sprite id="basilisk_drone_01_injured" offsetX="-2" offsetY="45" ic="FF5E9C89" width="104" height="142" img="entity\beasts\basilisk_drone_01_injured.png" left="-48" right="44" top="-55" bottom="69" />
-<sprite id="basilisk_drone_head_01" offsetX="-2" offsetY="45" ic="FF315B61" width="104" height="142" img="entity\beasts\basilisk_drone_head_01.png" left="4" right="76" top="-62" bottom="8" />
 <sprite id="basilisk_drone_head_01_dead" offsetX="5" offsetY="20" f="64F6" f1="-5" f2="-25" ic="FF315E5F" width="179" height="154" img="entity\beasts\basilisk_drone_head_01_dead.png" left="-70" right="30" top="-47" bottom="11" />
-<sprite id="basilisk_drone_01_injured_head" offsetX="5" offsetY="20" f="64F6" f1="-5" f2="-25" ic="FF315E5F" width="179" height="154" img="entity\beasts\basilisk_drone_01_injured_head.png" left="-70" right="30" top="-47" bottom="11" />
 <sprite id="basilisk_drone_01_dead_arrows" offsetX="5" offsetY="20" f="64FB" ic="FF474C4F" width="179" height="154" img="entity\beasts\bear_01_dead_arrows.png" left="-31" right="47" top="-2" bottom="48" />
 <sprite id="basilisk_drone_01_dead_javelin" offsetX="5" offsetY="20" f="64FB" ic="FF141B27" width="179" height="154" img="entity\beasts\bear_01_dead_javelin.png" left="-55" right="41" top="13" bottom="75" />
 
