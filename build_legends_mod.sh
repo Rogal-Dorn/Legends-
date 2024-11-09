d60802241eb147d468704d15b4964abe8593e18e
BreakyZ <z1rkel4o@gmail.com>
2024-11-09 21:11:10 +0200

    Update build script

--- build_legends_mod.sh
+++ build_legends_mod.sh
@@ -39,123 +39,130 @@ echo "Copying mod_legends to $BBDir\\mod_legends ..."
 cp -R mod_legends/. "$BBDir\\mod_legends"
 handleExit
 echo "Copying mod script files to $BBDir\\scripts ..."
-FILES=$(git diff --name-only `git merge-base origin/master HEAD` | grep '.nut\|.txt\|.css\|.js\|.html')
-while read -r line; do
-    if [[ "$line" == *.sh ]]; then
-        :
-    elif [[ "$line" == *.md ]]; then
-        :
-    elif [[ "$line" == *.py ]]; then
-        :
-    elif [[ "$line" == documentation/*.txt ]]; then
-        :
-    elif [[ "$line" == unpacked* ]]; then
-        :
-    elif [[ "$line" == gfx/*.png ]]; then
-        :
-    elif [[ "$line" == gfx/*.jpg ]]; then
-        :
-    elif [[ "$line" == sounds/*.wav ]]; then
-        :
-    elif [[ "$line" == *.cnut ]]; then
-        :
-    elif [ "$line" == ".gitignore" ]; then
-        :
-    elif [ "$line" == ".editorconfig" ]; then
-        :
-    elif [ "$line" == "scripts/mapgen/templates/world/worldmap_generator.nut" ]; then
-        :
-    elif [ "$line" == "scripts/contracts/contract.nut" ]; then
-        :
-    elif [ "$line" == "scripts/entity/world/combat_manager.nut" ]; then
-        :
-    elif [ "$line" == "scripts/states/tactical_state.nut" ]; then
-        :
-    elif [ "$line" == "scripts/entity/tactical/actor.nut" ]; then
-        :
-    elif [ "$line" == "scripts/skills/skill.nut" ]; then
-        :
-    elif [ "$line" == "scripts/skills/skill_container.nut" ]; then
-        :
-    elif [ "$line" == "scripts/entity/tactical/tactical_entity_manager.nut" ]; then
-        :
-    elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_bow.nut" ]; then
-        :
-    elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_crush_armor.nut" ]; then
-        :
-    elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_decapitate.nut" ]; then
-        :
-    elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_default.nut" ]; then
-        :
-    elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_knock_out.nut" ]; then
-        :
-    elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_lash.nut" ]; then
-        :
-    elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_swallow_whole.nut" ]; then
-        :
-    elif [ "$line" == "scripts/ai/tactical/behaviors/ai_hook.nut" ]; then
-        :
-    elif [ "$line" == "scripts/ai/tactical/behaviors/ai_boost_stamina.nut" ]; then
-        :
-    elif [ "$line" == "scripts/ai/tactical/behaviors/ai_charm.nut" ]; then
-        :
-    elif [ "$line" == "scripts/ai/tactical/behaviors/ai_defend_rotation.nut" ]; then
-        :
-    elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_terror.nut" ]; then
-        :
-    elif [ "$line" == "scripts/contracts/contracts/barbarian_king_contract" ]; then
-        :
-    elif [ "$line" == "scripts/factions/faction_action.nut" ]; then
-        :
-    elif [ "$line" == "scripts/skills/racial/alp_racial.nut" ]; then
-        :
-    elif [ "$line" == "scripts/events/events/dlc4/cultist_origin_vs_old_gods_event.nut" ]; then
-        :
-    elif [ "$line" == "scripts/config/world_entity_common.nut" ]; then
-        :
-    elif [ "$line" == "scripts/!mods_preload/!config/tnf_debugMode_lib.nut" ]; then
-        :
-    elif [ "$line" == "scripts/!mods_preload/tnf_debugMode.nut" ]; then
-        :
-    elif [ "$line" == "scripts/crafting/blueprints/paint_black_blueprint.nut" ]; then
-        :
-    elif [ "$line" == "scripts/crafting/blueprints/paint_orange_red_blueprint.nut" ]; then
-        :
-    elif [ "$line" == "scripts/crafting/blueprints/paint_red_blueprint.nut" ]; then
-        :
-    elif [ "$line" == "scripts/crafting/blueprints/paint_remover_blueprint.nut" ]; then
-        :
-    elif [ "$line" == "scripts/crafting/blueprints/paint_set_blueprint.nut" ]; then
-        :
-    elif [ "$line" == "scripts/crafting/blueprints/paint_white_blue_blueprint.nut" ]; then
-        :
-    elif [ "$line" == "scripts/crafting/blueprints/paint_white_green_yellow_blueprint.nut" ]; then
-        :
-    else
-        #echo "$line"
-        path=$( echo ${line%/*} )
-        mkdir -p "$BBDir\\$path"
-        handleExit
-        cp "$line" "$BBDir\\$line"
-        handleExit
-    fi
-done <<< "$FILES"
+cp -R scripts/. "$BBDir\\scripts"
+handleExit
+echo "Copying mod script files to $BBDir\\ui ..."
+cp -R ui/. "$BBDir\\ui"
+handleExit
+echo "Copying mod preload files to $BBDir\\preload ..."
+cp -R ui/. "$BBDir\\preload"
+handleExit
+# while read -r line; do
+#     if [[ "$line" == *.sh ]]; then
+#         :
+#     elif [[ "$line" == *.md ]]; then
+#         :
+#     elif [[ "$line" == *.py ]]; then
+#         :
+#     elif [[ "$line" == documentation/*.txt ]]; then
+#         :
+#     elif [[ "$line" == unpacked* ]]; then
+#         :
+#     elif [[ "$line" == gfx/*.png ]]; then
+#         :
+#     elif [[ "$line" == gfx/*.jpg ]]; then
+#         :
+#     elif [[ "$line" == sounds/*.wav ]]; then
+#         :
+#     elif [[ "$line" == *.cnut ]]; then
+#         :
+#     elif [ "$line" == ".gitignore" ]; then
+#         :
+#     elif [ "$line" == ".editorconfig" ]; then
+#         :
+#     elif [ "$line" == "scripts/mapgen/templates/world/worldmap_generator.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/contracts/contract.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/entity/world/combat_manager.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/states/tactical_state.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/entity/tactical/actor.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/skills/skill.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/skills/skill_container.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/entity/tactical/tactical_entity_manager.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_bow.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_crush_armor.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_decapitate.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_default.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_knock_out.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_lash.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_swallow_whole.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_hook.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_boost_stamina.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_charm.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_defend_rotation.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_terror.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/contracts/contracts/barbarian_king_contract" ]; then
+#         :
+#     elif [ "$line" == "scripts/factions/faction_action.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/skills/racial/alp_racial.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/events/events/dlc4/cultist_origin_vs_old_gods_event.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/config/world_entity_common.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/!mods_preload/!config/tnf_debugMode_lib.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/!mods_preload/tnf_debugMode.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/crafting/blueprints/paint_black_blueprint.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/crafting/blueprints/paint_orange_red_blueprint.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/crafting/blueprints/paint_red_blueprint.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/crafting/blueprints/paint_remover_blueprint.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/crafting/blueprints/paint_set_blueprint.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/crafting/blueprints/paint_white_blue_blueprint.nut" ]; then
+#         :
+#     elif [ "$line" == "scripts/crafting/blueprints/paint_white_green_yellow_blueprint.nut" ]; then
+#         :
+#     else
+#         #echo "$line"
+#         path=$( echo ${line%/*} )
+#         mkdir -p "$BBDir\\$path"
+#         handleExit
+#         cp "$line" "$BBDir\\$line"
+#         handleExit
+#     fi
+# done <<< "$FILES"
 
-echo "Compiling all nut files ..."
-cd ../bin
-o=$(./masscompile.bat "$BBDir\\scripts")
-o=$(./masscompile.bat "$BBDir\\mod_legends")
-cd ../"$RepoDir"
+# echo "Compiling all nut files ..."
+# cd ../bin
+# o=$(./masscompile.bat "$BBDir\\scripts")
+# o=$(./masscompile.bat "$BBDir\\mod_legends")
+# cd ../"$RepoDir"
 
-checkForCompileError "$o"
-resp="$?"
-echo $resp
-if [ $resp -ne "0" ]
-then
-    echo "Failed to build Legends!"
-    exit 1
-else
-    echo "Success!"
-    exit 0
-fi
+# checkForCompileError "$o"
+# resp="$?"
+# echo $resp
+# if [ $resp -ne "0" ]
+# then
+#     echo "Failed to build Legends!"
+#     exit 1
+# else
+#     echo "Success!"
+#     exit 0
+# fi
 
