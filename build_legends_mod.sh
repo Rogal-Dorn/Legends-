b6171e9e818717458ed3cdb38957a5008229a550
chopeks <37939738+chopeks@users.noreply.github.com>
2024-12-18 21:14:32 +0100

    split artifact buildscript

--- build_legends_mod.sh
+++ build_legends_mod.sh
@@ -47,132 +47,17 @@ handleExit
 echo "Copying mod script files to $BuildDir\\scripts ..."
 cp -R scripts/. "$BuildDir\\scripts"
 handleExit
-echo "Copying mod script files to $BuildDir\\ui ..."
+echo "Copying mod ui files to $BuildDir\\ui ..."
 cp -R ui/. "$BuildDir\\ui"
 handleExit
 echo "Copying mod preload files to $BuildDir\\preload ..."
 cp -R ui/. "$BuildDir\\preload"
 handleExit
-# while read -r line; do
-#     if [[ "$line" == *.sh ]]; then
-#         :
-#     elif [[ "$line" == *.md ]]; then
-#         :
-#     elif [[ "$line" == *.py ]]; then
-#         :
-#     elif [[ "$line" == documentation/*.txt ]]; then
-#         :
-#     elif [[ "$line" == unpacked* ]]; then
-#         :
-#     elif [[ "$line" == gfx/*.png ]]; then
-#         :
-#     elif [[ "$line" == gfx/*.jpg ]]; then
-#         :
-#     elif [[ "$line" == sounds/*.wav ]]; then
-#         :
-#     elif [[ "$line" == *.cnut ]]; then
-#         :
-#     elif [ "$line" == ".gitignore" ]; then
-#         :
-#     elif [ "$line" == ".editorconfig" ]; then
-#         :
-#     elif [ "$line" == "scripts/mapgen/templates/world/worldmap_generator.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/contracts/contract.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/entity/world/combat_manager.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/states/tactical_state.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/entity/tactical/actor.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/skills/skill.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/skills/skill_container.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/entity/tactical/tactical_entity_manager.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_bow.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_crush_armor.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_decapitate.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_default.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_knock_out.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_lash.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_swallow_whole.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_hook.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_boost_stamina.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_charm.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_defend_rotation.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/ai/tactical/behaviors/ai_attack_terror.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/contracts/contracts/barbarian_king_contract" ]; then
-#         :
-#     elif [ "$line" == "scripts/factions/faction_action.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/skills/racial/alp_racial.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/events/events/dlc4/cultist_origin_vs_old_gods_event.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/config/world_entity_common.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/!mods_preload/!config/tnf_debugMode_lib.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/!mods_preload/tnf_debugMode.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/crafting/blueprints/paint_black_blueprint.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/crafting/blueprints/paint_orange_red_blueprint.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/crafting/blueprints/paint_red_blueprint.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/crafting/blueprints/paint_remover_blueprint.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/crafting/blueprints/paint_set_blueprint.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/crafting/blueprints/paint_white_blue_blueprint.nut" ]; then
-#         :
-#     elif [ "$line" == "scripts/crafting/blueprints/paint_white_green_yellow_blueprint.nut" ]; then
-#         :
-#     else
-#         #echo "$line"
-#         path=$( echo ${line%/*} )
-#         mkdir -p "$BuildDir\\$path"
-#         handleExit
-#         cp "$line" "$BuildDir\\$line"
-#         handleExit
-#     fi
-# done <<< "$FILES"
-
-# echo "Compiling all nut files ..."
-# cd ../bin
-# o=$(./masscompile.bat "$BuildDir\\scripts")
-# o=$(./masscompile.bat "$BuildDir\\mod_legends")
-# cd ../"$RepoDir"
-
-# checkForCompileError "$o"
-# resp="$?"
-# echo $resp
-# if [ $resp -ne "0" ]
-# then
-#     echo "Failed to build Legends!"
-#     exit 1
-# else
-#     echo "Success!"
-#     exit 0
-# fi
 
 # zip the content of build dir and move file to bb's /data dir
 cd "$BuildDir"
-7z a -tzip mod_legends.zip *
-mv mod_legends.zip "$BBDir"
+7z a -tzip mod_legends_assets.zip brushes gfx sounds
+7z a -tzip mod_legends_build.zip mod_legends preload scripts ui
+
+mv mod_legends_assets.zip "$BBDir"
+mv mod_legends_build.zip "$BBDir"
--- build_patch.sh
+++ build_patch.sh
@@ -1,10 +1,31 @@
-Source="${1-""}" # commit hash, branch or tag
-BBBuilder="${2-"BBBuilder.exe"}"
+#!/bin/bash
+# Save the most recent tag in current branch, that ends with .0
+latest_tag=$(git tag --list | grep -E '^\d+(\.\d+)*\.0$' | sort -V | tail -n 1)
 
-currentBranch=$(git rev-parse --abbrev-ref HEAD)
+# If no such tag exists, fall back to the most recent tag (for safety)
+if [ -z "$latest_tag" ]; then
+    latest_tag=$(git tag --sort=-creatordate | head -n 1)
+fi
+echo "using tag $latest_tag"
 
-[ -d "./.bbbuilder" ] && rm -r "./.bbbuilder"
+# Get the commit hash associated with the latest tag
+commit_hash=$(git rev-list -n 1 "$latest_tag")
 
-"$BBBuilder" build "./" -diff "$Source,$currentBranch"
+Source="${1-"$commit_hash"}" # commit hash o branch if you wanna overwrite
 
-[ -d "./.bbbuilder" ] && rm -r "./.bbbuilder"
+zip_archive="mod_legends_build-$(git tag --sort=-creatordate | head -n 1).zip"
+# Remove the existing zip archive if it exists
+if [ -f "$zip_archive" ]; then
+    rm "$zip_archive"
+fi
+
+7z a -tzip "$zip_archive" mod_legends preload scripts ui
+
+# Get a list of all files modified since that commit hash
+modified_files=$(git diff --name-only "$Source" HEAD)
+# Filter the files that are in 'gfx' or 'sounds' directories
+filtered_files=$(echo "$modified_files" | grep -E '^(gfx|sounds)/')
+# Add the filtered files into the existing zip archive
+echo "$filtered_files" | while read -r file; do
+    7z a "$zip_archive" "$file"
+done
